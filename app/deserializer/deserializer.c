a6ac3e98d73e50acac5d83338dd013da38ef7f02
Monika Singh <moninom@amazon.com>
2024-01-04 08:23:01 +0000

    Update app

--- app/deserializer/deserializer.c
+++ app/deserializer/deserializer.c
@@ -8,7 +8,6 @@
 int main( void )
 {
     StunResult_t result = STUN_RESULT_OK;
-    uint8_t transactionId[STUN_HEADER_TRANSACTION_ID_LENGTH];
     StunContext_t stunDContext;
     StunHeader_t stunHeader;
     StunAttribute_t stunAttribute;
@@ -19,51 +18,54 @@ int main( void )
     uint32_t priority, expectedPriority = 0x7E7F00FF;
     
 
-    uint8_t serializesMessage[] = {  0x00, 0x01, 0x00, 0x4c, 0x21, 0x12, 0xa4, 0x42, 0x21, 0x8d,
+    uint8_t serializesMessage[] = {  0x00, 0x01, 0x00, 0x20, 0x21, 0x12, 0xa4, 0x42, 0x21, 0x8d,
                                      0x70, 0xf0, 0x9c, 0xcd, 0x89, 0x06, 0x62, 0x25, 0x89, 0x97, 
                                      0x00, 0x06, 0x00, 0x11, 0x36, 0x61, 0x30, 0x35, 0x66, 0x38, 0x34, 0x38,
                                      0x3a, 0x38, 0x61, 0x63, 0x33, 0x65, 0x39, 0x30, 0x32, 0x00, 0x00, 0x00, 
                                      0x00, 0x24, 0x00, 0x04, 0x7e, 0x7f, 0x00, 0xff };
+
     stunMessageLength = sizeof(serializesMessage);
 
     /* --------------------- Deserialisation --------------------- */
 
     result = StunDeserializer_Init( &stunDContext, serializesMessage, stunMessageLength);
 
-    if(result == STUN_RESULT_OK)
+    if( result == STUN_RESULT_OK )
     {  
         result = StunDeserializer_GetHeader( &stunDContext, &stunHeader );
+        printf("Stun messageType = %d\n",stunHeader.messageType);
+        printf("Stun messageLength = %d\n\n",stunHeader.messageLength);
     }
 
-    while( stunDContext.currentIndex < stunDContext.totalLength && result == STUN_RESULT_OK)
-        {
-            result = StunDeserializer_GetNextAttribute(&stunDContext, &stunAttribute);
-            printf("Type %x ValLen %x\n", stunAttribute.attributeType, stunAttribute.attributeValueLength);
+    while( result == STUN_RESULT_OK && stunDContext.currentIndex < stunDContext.totalLength )
+    {
+        result = StunDeserializer_GetNextAttribute(&stunDContext, &stunAttribute);
+        printf("Type %x ValLen %x\n", stunAttribute.attributeType, stunAttribute.attributeValueLength);
             
-            if( result == STUN_RESULT_OK)
+        if( result == STUN_RESULT_OK)
+        {
+            switch ( stunAttribute.attributeType )
             {
-                switch ( stunAttribute.attributeType )
-                {
-                    case STUN_ATTRIBUTE_TYPE_USERNAME:
+                case STUN_ATTRIBUTE_TYPE_USERNAME:
                         result = StunDeserializer_ParseAttributeUsername((const StunAttribute_t *)&stunAttribute, (const char **)&userName, &userNameLength);
                         if(!memcmp(userName, expectedUserName, stunAttribute.attributeValueLength))
                             printf("STUN_ATTRIBUTE_TYPE_USERNAME %s\n", userName);
                         break;
 
-                    case STUN_ATTRIBUTE_TYPE_PRIORITY:
+                case STUN_ATTRIBUTE_TYPE_PRIORITY:
                         result = StunDeserializer_ParseAttributePriority(&stunAttribute, &priority);
                         if(priority == expectedPriority)
                             printf("STUN_ATTRIBUTE_TYPE_PRIORITY %x\n", priority);
                         break;
                     
-                    default:
-                        // Skip over the unknown attributes
+                default:
+                        // Skip over the Unknown/Other attributes
                         break;
-                }
             }
         }
+    }
     
-    if(result == STUN_RESULT_OK )
+    if( result == STUN_RESULT_OK )
     {
         printf("\n\n----Result : Test Passed----\n\n");
     }
--- app/serializer/serializer.c
+++ app/serializer/serializer.c
@@ -5,49 +5,38 @@
 #include "stun_serializer.h"
 
 
-void printBuffer(char * pBuffer, size_t bufferLength)
-{
-    for(int i=0;i<bufferLength;i++)
-    {
-        printf("0x%02x ",pBuffer[i]);
-    }
-    printf("\n");
-}
-
 int main( void )
 {
     StunResult_t result = STUN_RESULT_OK;
     StunContext_t stunContext;
     size_t bufferLength=0, stunMessageLength;
     uint8_t * pBuffer, transactionId[STUN_HEADER_TRANSACTION_ID_LENGTH];
-    uint32_t priority = 10;
+    uint32_t priority = 0x7E7F00FF;
     StunHeader_t stunHeader;
     StunAttribute_t stunAttribute;
     const uint8_t * pStunMessage;
-    char *userName = "Monika";
+    char *userName = "6a05f848:8ac3e902";
     uint16_t usernameLen = strlen(userName);
 
     memcpy(transactionId, (char *) "ABCDEFGHIJKL", STUN_HEADER_TRANSACTION_ID_LENGTH);
 
     //Calculate required buffer size
     bufferLength = STUN_HEADER_LENGTH + STUN_ATTRIBUTE_TOTAL_LENGTH(sizeof(priority)) + STUN_ATTRIBUTE_TOTAL_LENGTH(ALIGN_SIZE_TO_WORD(usernameLen)); // HEADER + Priority Attribute  + UserName Attribute
-    printf("BufferLength used for %ld\n", bufferLength);
 
     pBuffer = malloc(bufferLength);
     if( pBuffer == NULL )
     {
-        printf("Buffer Allocation Failed");
         result = STUN_RESULT_OUT_OF_MEMORY;
     }
 
     /* --------------------- Serialisation --------------------- */
 
-    if(result == STUN_RESULT_OK)
+    if ( result == STUN_RESULT_OK )
     {
         result = StunSerializer_Init( &stunContext, pBuffer, bufferLength );
     }
 
-    if(result == STUN_RESULT_OK)
+    if ( result == STUN_RESULT_OK )
     {  
         stunHeader.messageType = STUN_MESSAGE_TYPE_BINDING_REQUEST;
         memcpy( stunHeader.transactionId, transactionId, STUN_HEADER_TRANSACTION_ID_LENGTH );
@@ -56,28 +45,30 @@ int main( void )
         result = StunSerializer_AddHeader( &stunContext, &stunHeader );
     }
 
-    if(result == STUN_RESULT_OK)
+    if ( result == STUN_RESULT_OK )
     {
-        result = StunSerializer_AddAttributePriority( &stunContext, 10 );
+        result = StunSerializer_AddAttributePriority( &stunContext, priority );
     }
 
-    if(result == STUN_RESULT_OK)
+    if ( result == STUN_RESULT_OK )
     {
         result = StunSerializer_AddAttributeUsername( &stunContext, userName, usernameLen );
     }
     
-    if(result == STUN_RESULT_OK)
+    if ( result == STUN_RESULT_OK )
     {
         result = StunSerializer_Finalize( &stunContext, &pStunMessage, &stunMessageLength );
     }
     
-    
-    printf("Serialised Message Length %ld\n",stunMessageLength );
-    printf("Serialised Message :\n" );
-    printBuffer(pBuffer, stunMessageLength);
-
-    if(result == STUN_RESULT_OK)
+    if ( result == STUN_RESULT_OK )
     {
+        printf("Serialised Message Length %ld\n",stunMessageLength );
+        printf("Serialised Message :\n" );
+        for( int i=0 ; i < stunMessageLength; i++ )
+        {
+            printf("0x%02x ", pStunMessage[i] );
+        }
+
         printf("\n\n----Result : Test Passed----\n\n");
     }
     else
