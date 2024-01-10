a4d3679b512652ebcb9739f07fc13c11e63cb1f5
Gaurav Aggarwal <aggarg@amazon.com>
2024-01-10 18:37:20 +0000

    Update deserializer app
    Signed-off-by: Gaurav Aggarwal <aggarg@amazon.com>

--- app/deserializer/deserializer.c
+++ app/deserializer/deserializer.c
@@ -1,77 +1,85 @@
-
+/* Standard includes. */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+/* Deserializer includes. */
 #include "stun_deserializer.h"
 
+uint8_t stunMessage[] = { 0x00, 0x01, 0x00, 0x20, 0x21, 0x12, 0xa4, 0x42, 0x21, 0x8d,
+                          0x70, 0xf0, 0x9c, 0xcd, 0x89, 0x06, 0x62, 0x25, 0x89, 0x97,
+                          0x00, 0x06, 0x00, 0x11, 0x36, 0x61, 0x30, 0x35, 0x66, 0x38, 0x34, 0x38,
+                          0x3a, 0x38, 0x61, 0x63, 0x33, 0x65, 0x39, 0x30, 0x32, 0x00, 0x00, 0x00,
+                          0x00, 0x24, 0x00, 0x04, 0x7e, 0x7f, 0x00, 0xff };
 
 int main( void )
 {
-    StunResult_t result = STUN_RESULT_OK;
-    StunContext_t stunDContext;
-    StunHeader_t stunHeader;
+    StunResult_t result;
+    StunContext_t stunContext;
+    StunHeader_t header;
     StunAttribute_t stunAttribute;
-    size_t stunMessageLength;
-    uint8_t *userName;
-    uint8_t expectedUserName[]="6a05f848:8ac3e902";
-    uint16_t userNameLength;
-    uint32_t priority, expectedPriority = 0x7E7F00FF;
-    
 
-    uint8_t serializesMessage[] = {  0x00, 0x01, 0x00, 0x20, 0x21, 0x12, 0xa4, 0x42, 0x21, 0x8d,
-                                     0x70, 0xf0, 0x9c, 0xcd, 0x89, 0x06, 0x62, 0x25, 0x89, 0x97, 
-                                     0x00, 0x06, 0x00, 0x11, 0x36, 0x61, 0x30, 0x35, 0x66, 0x38, 0x34, 0x38,
-                                     0x3a, 0x38, 0x61, 0x63, 0x33, 0x65, 0x39, 0x30, 0x32, 0x00, 0x00, 0x00, 
-                                     0x00, 0x24, 0x00, 0x04, 0x7e, 0x7f, 0x00, 0xff };
-
-    stunMessageLength = sizeof(serializesMessage);
-
-    /* --------------------- Deserialisation --------------------- */
-
-    result = StunDeserializer_Init( &stunDContext, serializesMessage, stunMessageLength);
+    /* Initialize STUN context for deserializing. */
+    result = StunDeserializer_Init( &( stunContext ),
+                                    &( stunMessage[ 0 ] ),
+                                    sizeof( stunMessage ),
+                                    &( header )  );
 
     if( result == STUN_RESULT_OK )
-    {  
-        result = StunDeserializer_GetHeader( &stunDContext, &stunHeader );
-        printf("Stun messageType = %d\n",stunHeader.messageType);
-        printf("Stun messageLength = %d\n\n",stunHeader.messageLength);
+    {
+        printf( "Stun Message Type = %d\n", header.messageType );
+        printf( "Transaction ID: " );
+        for( int i = 0; i < STUN_HEADER_TRANSACTION_ID_LENGTH; i++ )
+        {
+            printf( "0x%02x ", header.transactionId[ i ] );
+        }
+        printf( "\n" );
     }
 
-    while( result == STUN_RESULT_OK && stunDContext.currentIndex < stunDContext.totalLength )
+    while( result == STUN_RESULT_OK )
     {
-        result = StunDeserializer_GetNextAttribute(&stunDContext, &stunAttribute);
-        printf("Type %x ValLen %x\n", stunAttribute.attributeType, stunAttribute.attributeValueLength);
-            
-        if( result == STUN_RESULT_OK)
+        result = StunDeserializer_GetNextAttribute( &( stunContext ),
+                                                    &( stunAttribute ) );
+
+        if( result == STUN_RESULT_OK )
         {
-            switch ( stunAttribute.attributeType )
+            switch( stunAttribute.attributeType )
             {
                 case STUN_ATTRIBUTE_TYPE_USERNAME:
-                        result = StunDeserializer_ParseAttributeUsername((const StunAttribute_t *)&stunAttribute, (const char **)&userName, &userNameLength);
-                        if(!memcmp(userName, expectedUserName, stunAttribute.attributeValueLength))
-                            printf("STUN_ATTRIBUTE_TYPE_USERNAME %s\n", userName);
-                        break;
+                {
+                    const char * pUsername;
+                    uint16_t usernameLength;
+
+                    result = StunDeserializer_ParseAttributeUsername( &( stunAttribute ),
+                                                                        &( pUsername ),
+                                                                        &( usernameLength ) );
+
+                    if( result == STUN_RESULT_OK )
+                    {
+                        printf( "Username: %.*s\n", usernameLength, pUsername );
+                    }
+                }
+                break;
 
                 case STUN_ATTRIBUTE_TYPE_PRIORITY:
-                        result = StunDeserializer_ParseAttributePriority(&stunAttribute, &priority);
-                        if(priority == expectedPriority)
-                            printf("STUN_ATTRIBUTE_TYPE_PRIORITY %x\n", priority);
-                        break;
-                    
+                {
+                    uint32_t priority;
+
+                    result = StunDeserializer_ParseAttributePriority( &( stunAttribute ),
+                                                                      &( priority ) );
+
+                    if( result == STUN_RESULT_OK )
+                    {
+                        printf( "Priority: 0x%0X\n", priority );
+                    }
+                }
+                break;
+
                 default:
-                        // Skip over the Unknown/Other attributes
-                        break;
+                    break;
             }
         }
     }
-    
-    if( result == STUN_RESULT_OK )
-    {
-        printf("\n\n----Result : Test Passed----\n\n");
-    }
-    else
-    {
-        printf("\n\n----Result : Test Failed----\nreturn Val : %d\n",result );
-    }
+
     return 0;
 }
