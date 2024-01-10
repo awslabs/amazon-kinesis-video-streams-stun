73939f61f6ca36a309412a6d2ea51a517537a423
Gaurav Aggarwal <aggarg@amazon.com>
2024-01-10 17:23:41 +0000

    Add adding U32 attributes more efficient
    At the cost of code size.
    
    Signed-off-by: Gaurav Aggarwal <aggarg@amazon.com>

--- lib/source/stun_serializer.c
+++ lib/source/stun_serializer.c
@@ -5,27 +5,36 @@
 /* API includes. */
 #include "stun_serializer.h"
 
-static StunResult_t AddAttributeGeneric( StunContext_t * pCtx,
-                                         const StunAttribute_t * pAttribute );
+static StunResult_t AddAttributeBuffer( StunContext_t * pCtx,
+                                        StunAttributeType_t attributeType,
+                                        const uint8_t * pAttributeValueBuffer,
+                                        uint16_t attributeValueBufferLength );
+
+static StunResult_t AddAttributeU32( StunContext_t * pCtx,
+                                     StunAttributeType_t attributeType,
+                                     uint32_t attributeValue );
 /*-----------------------------------------------------------*/
 
-static StunResult_t AddAttributeGeneric( StunContext_t * pCtx,
-                                         const StunAttribute_t * pAttribute )
+static StunResult_t AddAttributeBuffer( StunContext_t * pCtx,
+                                        StunAttributeType_t attributeType,
+                                        const uint8_t * pAttributeValueBuffer,
+                                        uint16_t attributeValueBufferLength )
 {
     StunResult_t result = STUN_RESULT_OK;
-    uint16_t attributeLengthPadded;
+    uint16_t attributeValueLengthPadded;
 
     if( ( pCtx == NULL ) ||
-        ( pAttribute == NULL ) )
+        ( pAttributeValueBuffer == NULL ) ||
+        ( attributeValueBufferLength == 0 ) )
     {
         result = STUN_RESULT_BAD_PARAM;
     }
 
     if( result == STUN_RESULT_OK )
     {
-        attributeLengthPadded = ALIGN_SIZE_TO_WORD( pAttribute->attributeValueLength );
+        attributeValueLengthPadded = ALIGN_SIZE_TO_WORD( attributeValueBufferLength );
 
-        if( REMAINING_LENGTH( pCtx ) < STUN_ATTRIBUTE_TOTAL_LENGTH( attributeLengthPadded ) )
+        if( REMAINING_LENGTH( pCtx ) < STUN_ATTRIBUTE_TOTAL_LENGTH( attributeValueLengthPadded ) )
         {
             result = STUN_RESULT_OUT_OF_MEMORY;
         }
@@ -40,7 +49,7 @@ static StunResult_t AddAttributeGeneric( StunContext_t * pCtx,
             result = STUN_RESULT_INVALID_ATTRIBUTE_ORDER;
         }
         else if( ( ( pCtx->flags & STUN_FLAG_INTEGRITY_ATTRIBUTE ) != 0 ) &&
-                 ( pAttribute->attributeType != STUN_ATTRIBUTE_TYPE_FINGERPRINT ) )
+                 ( attributeType != STUN_ATTRIBUTE_TYPE_FINGERPRINT ) )
         {
             /* No attribute other than fingerprint can be added after Integrity
              * attribute. */
@@ -51,35 +60,101 @@ static StunResult_t AddAttributeGeneric( StunContext_t * pCtx,
     if( result == STUN_RESULT_OK )
     {
         /* Update flags. */
-        if( pAttribute->attributeType == STUN_ATTRIBUTE_TYPE_FINGERPRINT )
+        if( attributeType == STUN_ATTRIBUTE_TYPE_FINGERPRINT )
         {
             pCtx->flags |= STUN_FLAG_FINGERPRINT_ATTRIBUTE;
         }
-        if( pAttribute->attributeType == STUN_ATTRIBUTE_TYPE_MESSAGE_INTEGRITY )
+        if( attributeType == STUN_ATTRIBUTE_TYPE_MESSAGE_INTEGRITY )
         {
             pCtx->flags |= STUN_FLAG_INTEGRITY_ATTRIBUTE;
         }
 
         /* Write Attribute type, length and value. */
         WRITE_UINT16( &( pCtx->pStart[ pCtx->currentIndex ] ),
-                      pAttribute->attributeType );
+                      attributeType );
 
         WRITE_UINT16( &( pCtx->pStart[ pCtx->currentIndex + STUN_ATTRIBUTE_HEADER_LENGTH_OFFSET ] ),
-                      pAttribute->attributeValueLength );
+                      attributeValueBufferLength );
 
         memcpy( ( void * ) &( pCtx->pStart[ pCtx->currentIndex + STUN_ATTRIBUTE_HEADER_VALUE_OFFSET ] ),
-                pAttribute->pAttributeValue,
-                pAttribute->attributeValueLength );
+                pAttributeValueBuffer,
+                attributeValueBufferLength );
 
         /* Zero out the padded bytes. */
-        if( attributeLengthPadded > pAttribute->attributeValueLength )
+        if( attributeValueLengthPadded > attributeValueBufferLength )
         {
-            memset( ( void * ) &( pCtx->pStart[ pCtx->currentIndex + STUN_ATTRIBUTE_TOTAL_LENGTH( pAttribute->attributeValueLength ) ] ),
+            memset( ( void * ) &( pCtx->pStart[ pCtx->currentIndex + STUN_ATTRIBUTE_TOTAL_LENGTH( attributeValueBufferLength ) ] ),
                     0,
-                    attributeLengthPadded - pAttribute->attributeValueLength );
+                    attributeValueLengthPadded - attributeValueBufferLength );
         }
 
-        pCtx->currentIndex += STUN_ATTRIBUTE_TOTAL_LENGTH( attributeLengthPadded );
+        pCtx->currentIndex += STUN_ATTRIBUTE_TOTAL_LENGTH( attributeValueLengthPadded );
+    }
+
+    return result;
+}
+/*-----------------------------------------------------------*/
+
+static StunResult_t AddAttributeU32( StunContext_t * pCtx,
+                                     StunAttributeType_t attributeType,
+                                     uint32_t attributeValue )
+{
+    StunResult_t result = STUN_RESULT_OK;
+    uint16_t attributeValueLength = sizeof( uint32_t );
+
+    if( pCtx == NULL )
+    {
+        result = STUN_RESULT_BAD_PARAM;
+    }
+
+    if( result == STUN_RESULT_OK )
+    {
+        if( REMAINING_LENGTH( pCtx ) < STUN_ATTRIBUTE_TOTAL_LENGTH( attributeValueLength ) )
+        {
+            result = STUN_RESULT_OUT_OF_MEMORY;
+        }
+    }
+
+    if( result == STUN_RESULT_OK )
+    {
+        if( ( pCtx->flags & STUN_FLAG_FINGERPRINT_ATTRIBUTE ) != 0 )
+        {
+            /* No more attributes can be added after Fingerprint - it must  be
+             * the last attribute. */
+            result = STUN_RESULT_INVALID_ATTRIBUTE_ORDER;
+        }
+        else if( ( ( pCtx->flags & STUN_FLAG_INTEGRITY_ATTRIBUTE ) != 0 ) &&
+                 ( attributeType != STUN_ATTRIBUTE_TYPE_FINGERPRINT ) )
+        {
+            /* No attribute other than fingerprint can be added after Integrity
+             * attribute. */
+            result = STUN_RESULT_INVALID_ATTRIBUTE_ORDER;
+        }
+    }
+
+    if( result == STUN_RESULT_OK )
+    {
+        /* Update flags. */
+        if( attributeType == STUN_ATTRIBUTE_TYPE_FINGERPRINT )
+        {
+            pCtx->flags |= STUN_FLAG_FINGERPRINT_ATTRIBUTE;
+        }
+        if( attributeType == STUN_ATTRIBUTE_TYPE_MESSAGE_INTEGRITY )
+        {
+            pCtx->flags |= STUN_FLAG_INTEGRITY_ATTRIBUTE;
+        }
+
+        /* Write Attribute type, length and value. */
+        WRITE_UINT16( &( pCtx->pStart[ pCtx->currentIndex ] ),
+                      attributeType );
+
+        WRITE_UINT16( &( pCtx->pStart[ pCtx->currentIndex + STUN_ATTRIBUTE_HEADER_LENGTH_OFFSET ] ),
+                      attributeValueLength );
+
+        WRITE_UINT32( &( pCtx->pStart[ pCtx->currentIndex + STUN_ATTRIBUTE_HEADER_VALUE_OFFSET ] ),
+                      attributeValue );
+
+        pCtx->currentIndex += STUN_ATTRIBUTE_TOTAL_LENGTH( attributeValueLength );
     }
 
     return result;
@@ -132,20 +207,9 @@ StunResult_t StunSerializer_Init( StunContext_t * pCtx,
 StunResult_t StunSerializer_AddAttributePriority( StunContext_t * pCtx,
                                                   uint32_t priority )
 {
-    StunResult_t result;
-    StunAttribute_t attribute;
-    uint32_t priorityUpdated;
-
-    WRITE_UINT32( &priorityUpdated, priority );
-
-    attribute.attributeType = STUN_ATTRIBUTE_TYPE_PRIORITY;
-    attribute.pAttributeValue = ( uint8_t * )&( priorityUpdated );
-    attribute.attributeValueLength = sizeof( priorityUpdated );
-
-    result = AddAttributeGeneric( pCtx,
-                                  &( attribute ) );
-
-    return result;
+    return AddAttributeU32( pCtx,
+                            STUN_ATTRIBUTE_TYPE_PRIORITY,
+                            priority );
 }
 /*-----------------------------------------------------------*/
 
@@ -153,19 +217,11 @@ StunResult_t StunSerializer_AddAttributeUsername( StunContext_t * pCtx,
                                                   const char * pUsername,
                                                   uint16_t usernameLength )
 {
-    StunResult_t result;
-    StunAttribute_t attribute;
-
-    attribute.attributeType = STUN_ATTRIBUTE_TYPE_USERNAME;
-    attribute.pAttributeValue = ( uint8_t * ) pUsername;
-    attribute.attributeValueLength = usernameLength;
-
-    result = AddAttributeGeneric( pCtx,
-                                  &( attribute ) );
-
-    return result;
+    return AddAttributeBuffer( pCtx,
+                               STUN_ATTRIBUTE_TYPE_USERNAME,
+                               ( const uint8_t * ) pUsername,
+                               usernameLength );
 }
-
 /*-----------------------------------------------------------*/
 
 StunResult_t StunSerializer_Finalize( StunContext_t * pCtx,
