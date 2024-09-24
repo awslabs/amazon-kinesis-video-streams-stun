c3ab267e6fcc3574afb1ba638ca743e59cd72052
Tiya Jain <tiyajain@amazon.com>
2024-09-24 12:00:45 +0530

    Add unit-tests for AddAttributeErrorCode function

--- test/unit-test/stun_serializer/stun_serializer_utest.c
+++ test/unit-test/stun_serializer/stun_serializer_utest.c
@@ -13,7 +13,7 @@
 
 /* ===========================  EXTERN VARIABLES  =========================== */
 
-#define MAX_BUFFER_LENGTH        20
+#define MAX_BUFFER_LENGTH        512
 
 void setUp( void )
 {
@@ -149,3 +149,318 @@ void test_StunSerializer_Init_BadParams( void )
 }
 
 /*-----------------------------------------------------------*/
+
+/**
+ * @brief Validate StunSerializer_AddAttributeErrorCode in the happy path.
+ */
+void test_StunSerializer_AddAttributeErrorCode_Pass_( void )
+{
+    StunContext_t ctx = { 0 };
+    StunResult_t result;
+    StunHeader_t header = { 0 };
+    uint8_t buffer[ MAX_BUFFER_LENGTH ] = { 0 };
+    uint8_t transactionId[ STUN_HEADER_TRANSACTION_ID_LENGTH ] = { 0x12, 0x34, 0x56,
+                                                                   0x78, 0x9A, 0xBC,
+                                                                   0xDE, 0xF0, 0xAB,
+                                                                   0xCD, 0xEF, 0xA5 };
+    const uint8_t * errorPhrase = ( const uint8_t * ) "Error Phrase";
+    uint16_t errorPhraseLength = strlen( ( const char * ) errorPhrase );
+    uint16_t errorCode = 0x0600;
+    size_t expectedLength;
+    uint8_t expectedStunMessage[] =
+    {
+        /* Message Type = STUN Binding Request, Message Length = 20. */
+        0x00, 0x01, 0x00, 0x14,
+        /* Magic cookie. */
+        0x21, 0x12, 0xA4, 0x42,
+        /* Transaction ID. */
+        0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC, 0xDE, 0xF0, 0xAB, 0xCD, 0xEF, 0xA5,
+        /* Attribute Type = Error Code, Attribute Length = 20 */
+        0x00, 0x09, 0x00, 0x10,
+        /* Reserved = 0, Error Code = 600 */
+        0x00, 0x00, 0x06, 0x00,
+        /* Error Phrase */
+        0x45, 0x72, 0x72, 0x6F, 0x72, 0x20, 0x50, 0x68, 0x72, 0x61, 0x73, 0x65
+    };
+    size_t expectedStunMessageLength = sizeof( expectedStunMessage );
+
+    header.messageType = STUN_MESSAGE_TYPE_BINDING_REQUEST;
+    header.pTransactionId = &( transactionId[ 0 ] );
+
+    result = StunSerializer_Init( &( ctx ),
+                                  &( buffer[0] ),
+                                  MAX_BUFFER_LENGTH,
+                                  &( header ) );
+
+    TEST_ASSERT_EQUAL( STUN_RESULT_OK,
+                       result );
+
+    result = StunSerializer_AddAttributeErrorCode( &( ctx ),
+                                                   errorCode,
+                                                   &( errorPhrase[0] ),
+                                                   errorPhraseLength );
+
+    TEST_ASSERT_EQUAL( STUN_RESULT_OK,
+                       result );
+
+    result = StunSerializer_Finalize( &( ctx ),
+                                      &( expectedLength ) );
+
+    TEST_ASSERT_EQUAL( STUN_RESULT_OK,
+                       result );
+
+    TEST_ASSERT_EQUAL_UINT8_ARRAY( expectedStunMessage,
+                                   buffer,
+                                   expectedStunMessageLength );
+
+    TEST_ASSERT_EQUAL( expectedLength,
+                       expectedStunMessageLength );
+}
+
+/*-----------------------------------------------------------*/
+
+/**
+ * @brief Validate StunSerializer_AddAttributeErrorCode with NULL context.
+ */
+void test_StunSerializer_AddAttributeErrorCode_NullContext( void )
+{
+    StunResult_t result;
+    const uint8_t * errorPhrase = ( const uint8_t * ) "Error Phrase";
+    uint16_t errorPhraseLength = strlen( ( const char * ) errorPhrase );
+    uint16_t errorCode = 0x0600;
+
+    result = StunSerializer_AddAttributeErrorCode( NULL,
+                                                   errorCode,
+                                                   &( errorPhrase[0] ),
+                                                   errorPhraseLength );
+
+    TEST_ASSERT_EQUAL( STUN_RESULT_BAD_PARAM,
+                       result );
+}
+
+/*-----------------------------------------------------------*/
+
+/**
+ * @brief Validate StunSerializer_AddAttributeErrorCode with NULL error phrase.
+ */
+void test_StunSerializer_AddAttributeErrorCode_NullErrorPhrase( void )
+{
+    StunContext_t ctx = { 0 };
+    StunResult_t result;
+    StunHeader_t header = { 0 };
+    uint8_t buffer[ MAX_BUFFER_LENGTH ] = { 0 };
+    uint8_t transactionId[ STUN_HEADER_TRANSACTION_ID_LENGTH ] = { 0x12, 0x34, 0x56,
+                                                                   0x78, 0x9A, 0xBC,
+                                                                   0xDE, 0xF0, 0xAB,
+                                                                   0xCD, 0xEF, 0xA5 };
+    uint16_t errorCode = 0x0600; 
+
+    header.messageType = STUN_MESSAGE_TYPE_BINDING_REQUEST;
+    header.pTransactionId = &( transactionId[ 0 ] );
+
+    result = StunSerializer_Init( &( ctx ),
+                                  &( buffer[0] ),
+                                  MAX_BUFFER_LENGTH,
+                                  &( header ) );
+                                  
+    TEST_ASSERT_EQUAL( STUN_RESULT_OK,
+                       result );
+
+    result = StunSerializer_AddAttributeErrorCode( &( ctx ),
+                                                   errorCode,
+                                                   NULL,
+                                                   0 );
+
+    TEST_ASSERT_EQUAL( STUN_RESULT_BAD_PARAM,
+                       result );
+}
+
+/*-----------------------------------------------------------*/
+
+/**
+ * @brief Validate StunSerializer_AddAttributeErrorCode with zero error phrase length.
+ */
+void test_StunSerializer_AddAttributeErrorCode_ZeroErrorPhraseLength( void )
+{
+    StunContext_t ctx = { 0 };
+    StunResult_t result;
+    StunHeader_t header = { 0 };
+    uint8_t buffer[ MAX_BUFFER_LENGTH ] = { 0 };
+    uint8_t transactionId[ STUN_HEADER_TRANSACTION_ID_LENGTH ] = { 0x12, 0x34, 0x56,
+                                                                   0x78, 0x9A, 0xBC,
+                                                                   0xDE, 0xF0, 0xAB,
+                                                                   0xCD, 0xEF, 0xA5 };
+    const uint8_t * errorPhrase = ( const uint8_t * ) "Error Phrase";
+    uint16_t errorCode = 0x0600; 
+
+    header.messageType = STUN_MESSAGE_TYPE_BINDING_REQUEST;
+    header.pTransactionId = &( transactionId[ 0 ] );
+
+    result = StunSerializer_Init( &( ctx ),
+                                  &( buffer[0] ),
+                                  MAX_BUFFER_LENGTH,
+                                  &( header ) );
+
+    TEST_ASSERT_EQUAL( STUN_RESULT_OK,
+                       result );
+
+     result = StunSerializer_AddAttributeErrorCode( NULL,
+                                                   errorCode,
+                                                   &( errorPhrase[0] ),
+                                                   0);
+
+    TEST_ASSERT_EQUAL( STUN_RESULT_BAD_PARAM,
+                       result );
+}
+
+/*-----------------------------------------------------------*/
+
+/**
+ * @brief Validate StunSerializer_AddAttributeErrorCode when pCtx->pStart is null.
+ */
+void test_StunSerializer_AddAttributeErrorCode_pStartNull( void )
+{
+    StunContext_t ctx = { 0 };
+    StunResult_t result;
+    StunHeader_t header = { 0 };
+    uint8_t buffer[ MAX_BUFFER_LENGTH ] = { 0 };
+    uint8_t transactionId[ STUN_HEADER_TRANSACTION_ID_LENGTH ] = { 0x12, 0x34, 0x56,
+                                                                   0x78, 0x9A, 0xBC,
+                                                                   0xDE, 0xF0, 0xAB,
+                                                                   0xCD, 0xEF, 0xA5 };
+    const uint8_t * errorPhrase = ( const uint8_t * ) "Error Phrase";
+    uint16_t errorPhraseLength = sizeof(errorPhrase );
+    uint16_t errorCode = 0x0600;
+
+    header.messageType = STUN_MESSAGE_TYPE_BINDING_REQUEST;
+    header.pTransactionId = &( transactionId[ 0 ] );
+
+     result = StunSerializer_Init( &( ctx ),
+                                  &( buffer[0] ),
+                                  MAX_BUFFER_LENGTH,
+                                  &( header ) );
+
+    TEST_ASSERT_EQUAL( STUN_RESULT_OK,
+                       result );
+
+    ctx.pStart = NULL; /* Set pCtx->pStart to NULL */
+
+    result = StunSerializer_AddAttributeErrorCode( &(ctx),
+                                                   errorCode,
+                                                   &(errorPhrase[0]),
+                                                   errorPhraseLength );
+                                                   
+    TEST_ASSERT_EQUAL( STUN_RESULT_OK,
+                       result );
+}
+
+/*-----------------------------------------------------------*/
+
+/**
+ * @brief Validate StunSerializer_AddAttributeErrorCode in case of out of memory.
+ */
+void test_StunSerializer_AddAttributeErrorCode_OutOfMemory( void )
+{
+    StunContext_t ctx = { 0 };
+    StunResult_t result;
+    StunHeader_t header = { 0 };
+    uint8_t buffer[ 20 ] = { 0 };
+    uint8_t transactionId[ STUN_HEADER_TRANSACTION_ID_LENGTH ] = { 0x12, 0x34, 0x56,
+                                                                   0x78, 0x9A, 0xBC,
+                                                                   0xDE, 0xF0, 0xAB,
+                                                                   0xCD, 0xEF, 0xA5 };
+    const uint8_t * errorPhrase = ( const uint8_t * ) "Error Phrase";
+    uint16_t errorPhraseLength = strlen( ( const char * ) errorPhrase );
+    uint16_t errorCode = 0x0600;
+
+    header.messageType = STUN_MESSAGE_TYPE_BINDING_REQUEST;
+    header.pTransactionId = &( transactionId[ 0 ] );
+
+    result = StunSerializer_Init( &( ctx ),
+                                  &( buffer[0] ),
+                                  20,
+                                  &( header ) );
+
+    TEST_ASSERT_EQUAL( STUN_RESULT_OK,
+                       result );
+
+    result = StunSerializer_AddAttributeErrorCode( &( ctx ),
+                                                   errorCode,
+                                                   &( errorPhrase[0] ),
+                                                   errorPhraseLength );
+
+    TEST_ASSERT_EQUAL( STUN_RESULT_OUT_OF_MEMORY,
+                       result );
+}
+
+/*-----------------------------------------------------------*/
+
+/**
+ * @brief Validate StunSerializer_AddAttributeErrorCode in case of short error phrase.
+ */
+void test_StunSerializer_AddAttributeErrorCode_Short( void )
+{
+    StunContext_t ctx = { 0 };
+    StunResult_t result;
+    StunHeader_t header = { 0 };
+    uint8_t buffer[ MAX_BUFFER_LENGTH ] = { 0 };
+    uint8_t transactionId[ STUN_HEADER_TRANSACTION_ID_LENGTH ] = { 0x12, 0x34, 0x56,
+                                                                   0x78, 0x9A, 0xBC,
+                                                                   0xDE, 0xF0, 0xAB,
+                                                                   0xCD, 0xEF, 0xA5 };
+    const uint8_t * errorPhrase = ( const uint8_t * ) "Short";
+    uint16_t errorPhraseLength = strlen( ( const char * ) errorPhrase );
+    uint16_t errorCode = 0x0600;
+    size_t expectedLength;
+    uint8_t expectedStunMessage[] =
+    {
+        /* Message Type = STUN Binding Request, Message Length = 20. */
+        0x00, 0x01, 0x00, 0x10,
+        /* Magic cookie. */
+        0x21, 0x12, 0xA4, 0x42,
+        /* Transaction ID. */
+        0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC, 0xDE, 0xF0, 0xAB, 0xCD, 0xEF, 0xA5,
+        /* Attribute Type = Error Code, Attribute Length = 20 */
+        0x00, 0x09, 0x00, 0x09,
+        /* Reserved = 0, Error Code = 600 */
+        0x00, 0x00, 0x06, 0x00,
+        /* Error Phrase */
+        0x53, 0x68, 0x6F, 0x72, 0x74, 0x00, 0x00, 0x00
+    };
+    size_t expectedStunMessageLength = sizeof( expectedStunMessage );
+
+    header.messageType = STUN_MESSAGE_TYPE_BINDING_REQUEST;
+    header.pTransactionId = &( transactionId[ 0 ] );
+
+    result = StunSerializer_Init( &( ctx ),
+                                  &( buffer[0] ),
+                                  MAX_BUFFER_LENGTH,
+                                  &( header ) );
+
+    TEST_ASSERT_EQUAL( STUN_RESULT_OK,
+                       result );
+
+    result = StunSerializer_AddAttributeErrorCode( &( ctx ),
+                                                   errorCode,
+                                                   &( errorPhrase[0] ),
+                                                   errorPhraseLength );
+
+    TEST_ASSERT_EQUAL( STUN_RESULT_OK,
+                       result );
+
+    result = StunSerializer_Finalize( &( ctx ),
+                                      &( expectedLength ) );
+
+    TEST_ASSERT_EQUAL( STUN_RESULT_OK,
+                       result );
+
+    TEST_ASSERT_EQUAL_UINT8_ARRAY( expectedStunMessage,
+                                   buffer,
+                                   expectedStunMessageLength );
+
+    TEST_ASSERT_EQUAL( expectedLength,
+                       expectedStunMessageLength );
+}
+
+/*-----------------------------------------------------------*/
+
